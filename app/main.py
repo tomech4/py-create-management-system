f3d5b82227e5d0fe17498d4c32be1dbc07851d03
tomech <109527747+tomech4@users.noreply.github.com>
2026-01-21 23:34:13 +0100

    fix birth_date attribute type in Student class

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class Specialty:
 class Student:
     first_name: str
     last_name: str
-    birth_date: str
+    birth_date: datetime
     average_mark: float
     has_scholarship: bool
     phone_number: int
