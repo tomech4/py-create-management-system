68ef39e586fdd2c43224965079f7eb73c45f04ea
tomech <109527747+tomech4@users.noreply.github.com>
2026-01-21 22:00:07 +0100

    implement classes and import necessary modules

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-# write your code here
+from dataclasses import dataclass
+from datetime import datetime
+import pickle
+
+
+@dataclass
+class Speciality:
+    name: str
+    number: int
+
+@dataclass
+class Student:
+    first_name: str
+    last_name: str
+    birth_date: str
+    average_mark: float
+    has_scholarship: bool
+    phone_number: int
+    address: str
+
+@dataclass
+class Group:
+    speciality: Speciality
+    course: str
+    students: list[Student]
